b330d0e498dbf7fc21c8fc744780667bb8393491
Claudia Pierson <c172@umbc.edu>
2020-02-03 08:38:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,16 +7,17 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
   def brand=(brand)
     if BRANDS include?(brand)
     @brand = brand 
     
     BRANDS <<  brand 
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+  
     
   end  
 end
